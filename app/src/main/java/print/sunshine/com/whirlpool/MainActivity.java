2a87f540a3e6eb3de5f905c89ba90734ebe31029
wangyungang <729201523@qq.com>
2017-03-15 09:28:59 +0800

    惠而浦第一次提交

---
+++ app/src/main/java/print/sunshine/com/whirlpool/MainActivity.java
@@ -0,0 +1,13 @@
+package print.sunshine.com.whirlpool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
